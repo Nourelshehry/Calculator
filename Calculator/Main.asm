fe859ee0df1b88cf366d8c5c7462094912c4b297
maha <maha170930@feng.bu.edu.eg>
2021-01-03 15:59:05 +0200

    update1

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -1,15 +1,19 @@
 INCLUDE Irvine32.inc
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, deExitCode:DWORD
 
 .data
    ;variables
 msg byte "insert the expression:",0 
 msg1 byte "=",0
 buffer byte 30 DUP(0)  ;array to get string from user          
-sign WORD 1            ;the sign of the number
-nr DWORD 0             
-co WORD 0              ; flag to know the operation (multiplication  or Division)
+sign   WORD 1            ;the sign of the number
+nr     DWORD 0             
+co     WORD 0              ; flag to know the operation (multiplication  or Division)
 result DWORD 0         
-aux DWORD 0            ; store the number that will be multiplied or divided
+aux    DWORD 0            ; store the number that will be multiplied or divided
 
 
 .code
