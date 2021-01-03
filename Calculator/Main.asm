8606971245c4df3b2491257dc1529da61476ac52
mayar <mayar.hanafy.14@gmail.com>
2021-01-03 15:40:43 +0200

    set registers

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -1,11 +1,18 @@
-.386
-.model flat, stdcall
-.stack 4096
-ExitProcess PROTO, deExitCode:DWORD
+INCLUDE Irvine32.inc
 
 .data
    ;variables
-
+msg byte "insert the expression:",0
+msg1 byte "=",0
+buffer byte 30 DUP(0)
+var DWORD ?
+sign WORD 1
+i DWORD 0
+n DWORD 0
+nr DWORD 0
+co WORD 0
+result DWORD 0
+aux DWORD 0
 
 
 .code
