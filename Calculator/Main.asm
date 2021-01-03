37be35e934bb46af4e0b605e249eac8268ec9786
mayar <mayar.hanafy.14@gmail.com>
2021-01-03 20:04:15 +0200

    comments of start & loop

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -16,16 +16,16 @@ aux    DWORD 0            ; store the number that will be multiplied or divided
 main PROC
 
 start : 
-  mov edx, offset msg
-  call WriteString
-  mov edx, offset buffer
-  mov ecx, sizeof buffer
-  call ReadString
-  mov ebp,0
+  mov edx, offset msg       ;get the offest of the variable msg
+  call WriteString          ;print the text in the variable msg to the user
+  mov edx, offset buffer    ;get the offest of buffer
+  mov ecx, sizeof buffer    ;put the size of the array in register ecx
+  call ReadString           ;take input from user
+  mov ebp,0                 ;put zero to ebp register ( index of the buffer array )
 
-loop1 :
-  cmp ebp, ecx
-  jae answer
+loop1 :                     ;while loop
+  cmp ebp, ecx              ;compare ebp  register (index) with ecx register (string length)
+  jae answer                ;if (ebp >= ecx) then jump to label answer  
 
 
 INVOKE ExitProcess, 0
