5e749b23ef538c9cb498ebe9b5a9f8fd1502e6cd
mayar <mayar.hanafy.14@gmail.com>
2021-01-03 15:48:24 +0200

    registers

--- Calculator/Main.asm
+++ Calculator/Main.asm
@@ -2,17 +2,14 @@ INCLUDE Irvine32.inc
 
 .data
    ;variables
-msg byte "insert the expression:",0
+msg byte "insert the expression:",0 
 msg1 byte "=",0
-buffer byte 30 DUP(0)
-var DWORD ?
-sign WORD 1
-i DWORD 0
-n DWORD 0
-nr DWORD 0
-co WORD 0
-result DWORD 0
-aux DWORD 0
+buffer byte 30 DUP(0)  ;array to get string from user          
+sign WORD 1            ;the sign of the number
+nr DWORD 0             
+co WORD 0              ; flag to know the operation (multiplication  or Division)
+result DWORD 0         
+aux DWORD 0            ; store the number that will be multiplied or divided
 
 
 .code
